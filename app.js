e58263763cecb061b1749ce6a886395f1f6b5c21
Brendan Ball <ball.brendan50@gmail.com>
2018-05-05 08:58:13 +0200

    some basic structure

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { graphqlExpress } from 'apollo-server-express'
+import morgan from 'morgan'
+import schema from './schema'
+const expressPlayground = require('graphql-playground-middleware-express').default
+
+const app = express()
+app.use(morgan('combined'))
+
+app.get('/graphql/playground', expressPlayground({ endpoint: 'graphql' }), (req, res) => { })
+app.use('/graphql', express.json(), graphqlExpress(req => ({ schema })))
+
+app.listen(3000, () => console.log('running graphql server on port 3000'))
